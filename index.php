e34a3ab20ae8737cc8a59b794304a06a23869570
WIlliam Tomé <william.tomenh@gmail.com>
2019-01-17 21:40:02 -0200

    Layout de interação com usuário criado.

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php 
 include 'contato.class.php';
+$contact = new contato();
+?>
+<h1>Contatos</h1>
 
-$contato = new contato();
+<a href="adicionar.php">[Adicionar]</a>
 
-//$contato->create("william@email.com");
-$contato->edit("William Weirich Tomé", "williamwt@gmail.com", 4);
+<br><br>
 
-$lista = $contato->getAll();
-echo "<pre>";
-print_r($lista);
-echo "</pre>";
+<table border="1" width="600">
+	<tr>
+		<th>ID</th>
+		<th>Nome</th>
+		<th>E-mail</th>
+		<th>Ações</th>
+	</tr>
+	
+	<?php
+	$list = $contact->getAll();
+	foreach ($list as $item):
+	?>
+	<tr>
+		<td><?= $item['id']; ?></td>
+		<td><?= $item['nome']; ?></td>
+		<td><?= $item['email']; ?></td>
+		<td>
+			<a href="editar.php?id=<?= $item['id']; ?>">[Editar]</a>
+			<a href="excluir.php?id=<?= $item['id']; ?>">[Excluir]</a>
+		</td>
+	</tr>
+	<?php endforeach ?>
+</table>
